bde976a56376cd5a5674e9459f1e02421bd06237
agolubev <andpgolubev@gmail.com>
2020-04-09 11:36:11 +0300

    Put OpenCL into .cl file

---
+++ stereo_disparity/stereo_disparity_kernels.cl
@@ -0,0 +1,180 @@
+// TODO: optimize indices in box_blur and other kernels
+
+// helper functions:
+int fix(int v, int max_v) {
+    if (v < 0) {
+        return -v;
+    } else if (v > max_v) {
+        const int diff = (v - max_v);
+        return max_v - diff;
+    }
+    return v;
+}
+bool out_of_bounds(int x, int l, int r) { return x < l || x > r; }
+
+// defines (the values are replaced in runtime to ensure consitency between C++ and OpenCL)
+#define EPS <re:EPS>
+#define UNKNOWN_DISPARITY <re:UNKNOWN_DISPARITY>
+#define MAX_WINDOW <re:MAX_WINDOW>
+#define MAX_BORDER <re:MAX_BORDER>
+
+// kernels and subroutines:
+__kernel void box_blur(__global uchar* out, __global const uchar* in, int rows, int cols,
+                       int k_size) {
+    const int idx_i = get_global_id(0);  // pixel index for row
+    const int idx_j = get_global_id(1);  // pixel index for col
+
+    // skip extra work items
+    if (out_of_bounds(idx_i, 0, rows - 1) || out_of_bounds(idx_j, 0, cols - 1)) {
+        return;
+    }
+
+    const int center_shift = (k_size - 1) / 2;
+
+    // prepare lines in advance
+    __global const uchar* in_lines[MAX_WINDOW] = {};
+    for (int i = 0; i < k_size; ++i) {
+        const int ii = idx_i + i - center_shift + MAX_BORDER;
+        in_lines[i] = in + ii * cols;
+    }
+
+    // prepare column indices in advance
+    int js[MAX_WINDOW] = {};
+    for (int j = 0; j < k_size; ++j) {
+        js[j] = fix(idx_j + j - center_shift, cols - 1);
+    }
+
+    // main loop
+    uint sum = 0;
+    for (int i = 0; i < k_size; ++i) {
+        for (int j = 0; j < k_size; ++j) {
+            sum += in_lines[i][js[j]];
+        }
+    }
+
+    const double multiplier = 1.0 / (k_size * k_size);
+    out[idx_i * cols + idx_j] = round(multiplier * sum);
+}
+
+double zncc(__global const uchar* left, uchar l_mean, __global const uchar* right, uchar r_mean,
+            int rows, int cols, int k_size, int idx_i, int idx_j, int d) {
+    const int center_shift = (k_size - 1) / 2;
+
+    int sum = 0;
+    double std_left = 0.0, std_right = 0.0;
+
+    // prepare lines in advance
+    __global const uchar* left_lines[MAX_WINDOW] = {};
+    __global const uchar* right_lines[MAX_WINDOW] = {};
+    for (int i = 0; i < k_size; ++i) {
+        const int ii = idx_i + i - center_shift + MAX_BORDER;
+        left_lines[i] = left + ii * cols;
+        right_lines[i] = right + ii * cols;
+    }
+
+    // prepare column indices in advance
+    int js1[MAX_WINDOW] = {};
+    int js2[MAX_WINDOW] = {};
+    for (int j = 0; j < k_size; ++j) {
+        const int jj = idx_j + j - center_shift;
+        js1[j] = fix(jj, cols - 1);
+        js2[j] = fix(jj + d, cols - 1);
+    }
+
+    // main loop
+    for (int i = 0; i < k_size; ++i) {
+        // prefetch window
+        __global const uchar* left_line = left_lines[i];
+        prefetch(left_line + js1[0], k_size);
+        __global const uchar* right_line = right_lines[i];
+        prefetch(right_line + js2[0], k_size);
+
+        // compute metric
+        for (int j = 0; j < k_size; ++j) {
+            const int left_pixel = (int)(left_line[js1[j]]) - (int)(l_mean);
+            const int right_pixel = (int)(right_line[js2[j]]) - (int)(r_mean);
+
+            sum += left_pixel * right_pixel;
+
+            std_left += pow((double)left_pixel, 2);
+            std_right += pow((double)right_pixel, 2);
+        }
+    }
+
+    // ensure STD DEV >= EPS (otherwise we get Inf)
+    std_left = max(sqrt(std_left), EPS);
+    std_right = max(sqrt(std_right), EPS);
+
+    const double dsum = sum;
+    return dsum / (std_left * std_right);
+}
+
+__kernel void make_disparity_map(__global uchar* out, __global const uchar* left,
+                                 __global const uchar* left_mean, __global const uchar* right,
+                                 __global const uchar* right_mean, int rows, int cols,
+                                 int window_size, int d_first, int d_last) {
+    const int idx_i = get_global_id(0);  // pixel index for row
+    const int idx_j = get_global_id(1);  // pixel index for col
+
+    // skip extra work items
+    if (out_of_bounds(idx_i, 0, rows - 1) || out_of_bounds(idx_j, 0, cols - 1)) {
+        return;
+    }
+
+    int best_disparity = 0;
+    // find max zncc and corresponding disparity for current pixel:
+    double max_zncc = -1.0;  // zncc in range [-1, 1]
+
+    uchar l_mean = left_mean[idx_i * cols + idx_j];
+    for (int d = d_first; d <= d_last; ++d) {
+        uchar r_mean = right_mean[idx_i * cols + fix(idx_j + d, cols - 1)];
+        double v = zncc(left, l_mean, right, r_mean, rows, cols, window_size, idx_i, idx_j, d);
+        if (max_zncc < v) {
+            max_zncc = v;
+            best_disparity = d;
+        }
+    }
+
+    // store absolute value of disparity
+    out[idx_i * cols + idx_j] = abs(best_disparity);
+}
+
+__kernel void cross_check_disparity(__global uchar* l2r, __global const uchar* r2l, int rows,
+                                    int cols, int disparity) {
+    const int idx_i = get_global_id(0);  // pixel index for row
+    const int idx_j = get_global_id(1);  // pixel index for col
+
+    // skip extra work items
+    if (out_of_bounds(idx_i, 0, rows - 1) || out_of_bounds(idx_j, 0, cols - 1)) {
+        return;
+    }
+
+    const int threshold = disparity / 4;
+
+    const int l2r_pixel = l2r[idx_i * cols + idx_j];
+    const int r2l_pixel = r2l[idx_i * cols + idx_j];
+    if (abs(l2r_pixel - r2l_pixel) > threshold) {
+        l2r[idx_i * cols + idx_j] = min(l2r_pixel, r2l_pixel);
+    }
+}
+
+__kernel void fill_occlusions_disparity(__global uchar* data, int rows, int cols, int disparity) {
+    const int idx_i = get_global_id(0);  // pixel index for row
+
+    // skip extra work items
+    if (out_of_bounds(idx_i, 0, rows - 1)) {
+        return;
+    }
+
+    // just pick closest non-zero value along current row
+
+    uchar nearest_intensity = 0;
+    for (int idx_j = 0; idx_j < cols; ++idx_j) {
+        const uchar pixel = data[idx_i * cols + idx_j];
+        if (pixel == UNKNOWN_DISPARITY) {
+            data[idx_i * cols + idx_j] = nearest_intensity;
+        } else {
+            nearest_intensity = pixel;
+        }
+    }
+}
--- stereo_disparity/stereo_disparity_ocl_inl.hpp
+++ stereo_disparity/stereo_disparity_ocl_inl.hpp
@@ -16,207 +16,48 @@
 #include "stereo_disparity_cpp_opt_inl.hpp"  // TODO: remove this include
 
 namespace stereo_ocl_base {
-// TODO: optimize indices in box_blur and other kernels
-std::string program_str(R"(
-// helper functions:
-int fix(int v, int max_v) {
-    if (v < 0) {
-        return -v;
-    } else if (v > max_v) {
-        const int diff = (v - max_v);
-        return max_v - diff;
-    }
-    return v;
+inline std::string read_opencl_program() {
+    static const std::string program_path = []() -> std::string {
+        char delim =
+#ifndef _WIN32
+            '/';
+#else
+            '\\';
+#endif
+        const char* last_delimiter_pos = std::strrchr(__FILE__, delim);
+        REQUIRE(last_delimiter_pos != nullptr);
+        std::string root_dir(__FILE__, std::distance(__FILE__, last_delimiter_pos + 1));
+        return root_dir + "stereo_disparity_kernels.cl";
+    }();
+    // TODO: the following is Linux specific:
+    std::ifstream program_file(program_path);
+    return std::string(std::istreambuf_iterator<char>(program_file),
+                       std::istreambuf_iterator<char>());
 }
-bool out_of_bounds(int x, int l, int r) {
-    return x < l || x > r;
-}
-
-// defines:
-#define EPS <re:EPS>
-#define UNKNOWN_DISPARITY <re:UNKNOWN_DISPARITY>
-#define MAX_WINDOW <re:MAX_WINDOW>
-#define MAX_BORDER <re:MAX_BORDER>
-
-// kernels and subroutines:
-__kernel void box_blur(__global uchar* out, __global const uchar* in, int rows, int cols,
-                       int k_size) {
-    const int idx_i = get_global_id(0);  // pixel index for row
-    const int idx_j = get_global_id(1);  // pixel index for col
-
-    // skip extra work items
-    if (out_of_bounds(idx_i, 0, rows - 1) || out_of_bounds(idx_j, 0, cols - 1)) {
-        return;
-    }
-
-    const int center_shift = (k_size - 1) / 2;
-
-    // prepare lines in advance
-    __global const uchar* in_lines[MAX_WINDOW] = {};
-    for (int i = 0; i < k_size; ++i) {
-        const int ii = idx_i + i - center_shift + MAX_BORDER;
-        in_lines[i] = in + ii * cols;
-    }
-
-    // prepare column indices in advance
-    int js[MAX_WINDOW] = {};
-    for (int j = 0; j < k_size; ++j) {
-        js[j] = fix(idx_j + j - center_shift, cols - 1);
-    }
-
-    // main loop
-    uint sum = 0;
-    for (int i = 0; i < k_size; ++i) {
-        for (int j = 0; j < k_size; ++j) {
-            sum += in_lines[ i ][ js[j] ];
-        }
-    }
-
-    const double multiplier = 1.0 / (k_size * k_size);
-    out[idx_i * cols + idx_j] = round(multiplier * sum);
-}
-
-double zncc(__global const uchar* left, uchar l_mean, __global const uchar* right, uchar r_mean,
-            int rows, int cols, int k_size, int idx_i, int idx_j, int d) {
-    const int center_shift = (k_size - 1) / 2;
-
-    int sum = 0;
-    double std_left = 0.0, std_right = 0.0;
-
-    // prepare lines in advance
-    __global const uchar* left_lines[MAX_WINDOW] = {};
-    __global const uchar* right_lines[MAX_WINDOW] = {};
-    for (int i = 0; i < k_size; ++i) {
-        const int ii = idx_i + i - center_shift + MAX_BORDER;
-        left_lines[i] = left + ii * cols;
-        right_lines[i] = right + ii * cols;
-    }
-
-    // prepare column indices in advance
-    int js1[MAX_WINDOW] = {};
-    int js2[MAX_WINDOW] = {};
-    for (int j = 0; j < k_size; ++j) {
-        const int jj = idx_j + j - center_shift;
-        js1[j] = fix(jj, cols - 1);
-        js2[j] = fix(jj + d, cols - 1);
-    }
-
-    // main loop
-    for (int i = 0; i < k_size; ++i) {
-        // prefetch window
-        __global const uchar* left_line = left_lines[i];
-        prefetch(left_line + js1[0], k_size);
-        __global const uchar* right_line = right_lines[i];
-        prefetch(right_line + js2[0], k_size);
-
-        // compute metric
-        for (int j = 0; j < k_size; ++j) {
-            const int left_pixel = (int)(left_line[ js1[j] ]) - (int)(l_mean);
-            const int right_pixel = (int)(right_line[ js2[j] ]) - (int)(r_mean);
-
-            sum += left_pixel * right_pixel;
-
-            std_left += pow((double)left_pixel, 2);
-            std_right += pow((double)right_pixel, 2);
-        }
-    }
-
-    // ensure STD DEV >= EPS (otherwise we get Inf)
-    std_left = max(sqrt(std_left), EPS);
-    std_right = max(sqrt(std_right), EPS);
-
-    const double dsum = sum;
-    return dsum / (std_left * std_right);
-}
-
-__kernel void make_disparity_map(__global uchar* out, __global const uchar* left,
-                                 __global const uchar* left_mean, __global const uchar* right,
-                                 __global const uchar* right_mean, int rows, int cols,
-                                 int window_size, int d_first, int d_last) {
-    const int idx_i = get_global_id(0);  // pixel index for row
-    const int idx_j = get_global_id(1);  // pixel index for col
-
-    // skip extra work items
-    if (out_of_bounds(idx_i, 0, rows - 1) || out_of_bounds(idx_j, 0, cols - 1)) {
-        return;
-    }
-
-    int best_disparity = 0;
-    // find max zncc and corresponding disparity for current pixel:
-    double max_zncc = -1.0;  // zncc in range [-1, 1]
-
-    uchar l_mean = left_mean[idx_i * cols + idx_j];
-    for (int d = d_first; d <= d_last; ++d) {
-        uchar r_mean = right_mean[idx_i * cols + fix(idx_j + d, cols - 1)];
-        double v = zncc(left, l_mean, right, r_mean, rows, cols, window_size, idx_i, idx_j, d);
-        if (max_zncc < v) {
-            max_zncc = v;
-            best_disparity = d;
-        }
-    }
-
-    // store absolute value of disparity
-    out[idx_i * cols + idx_j] = abs(best_disparity);
-}
-
-__kernel void cross_check_disparity(__global uchar* l2r, __global const uchar* r2l, int rows,
-                                    int cols, int disparity) {
-    const int idx_i = get_global_id(0);  // pixel index for row
-    const int idx_j = get_global_id(1);  // pixel index for col
-
-    // skip extra work items
-    if (out_of_bounds(idx_i, 0, rows - 1) || out_of_bounds(idx_j, 0, cols - 1)) {
-        return;
-    }
-
-    const int threshold = disparity / 4;
-
-    const int l2r_pixel = l2r[idx_i * cols + idx_j];
-    const int r2l_pixel = r2l[idx_i * cols + idx_j];
-    if (abs(l2r_pixel - r2l_pixel) > threshold) {
-        l2r[idx_i * cols + idx_j] = min(l2r_pixel, r2l_pixel);
-    }
-}
-
-__kernel void fill_occlusions_disparity(__global uchar* data, int rows, int cols, int disparity) {
-    const int idx_i = get_global_id(0);  // pixel index for row
-
-    // skip extra work items
-    if (out_of_bounds(idx_i, 0, rows - 1)) {
-        return;
-    }
-
-    // just pick closest non-zero value along current row
-
-    uchar nearest_intensity = 0;
-    for (int idx_j = 0; idx_j < cols; ++idx_j) {
-        const uchar pixel = data[idx_i * cols + idx_j];
-        if (pixel == UNKNOWN_DISPARITY) {
-            data[idx_i * cols + idx_j] = nearest_intensity;
-        } else {
-            nearest_intensity = pixel;
-        }
-    }
-}
-)");
 
 template<typename T> void replace_param(std::string& string, std::string search_for, T param) {
     std::regex re(search_for);
     string = std::regex_replace(string, re, std::to_string(param));
 }
 
-void set_parameters(std::string& ocl_program) {
+inline void set_parameters(std::string& ocl_program) {
     replace_param(ocl_program, "<re:EPS>", stereo_common::EPS);
     replace_param(ocl_program, "<re:UNKNOWN_DISPARITY>", stereo_common::UNKNOWN_DISPARITY);
     replace_param(ocl_program, "<re:MAX_WINDOW>", stereo_common::MAX_WINDOW);
     replace_param(ocl_program, "<re:MAX_BORDER>", stereo_common::MAX_BORDER);
 }
 
+std::string get_program() {
+    auto ocl_program = read_opencl_program();
+    set_parameters(ocl_program);
+    return ocl_program;
+}
+
 void _internal_stereo_compute_disparity(cv::Mat& out, const cv::Mat& left, const cv::Mat& right,
                                         int disparity, size_t platform_idx = 0,
                                         size_t device_idx = 0) {
     static OclExecutor e(
-        OclPrimitives(platform_idx, device_idx), program_str,
+        OclPrimitives(platform_idx, device_idx), get_program(),
         {"box_blur", "make_disparity_map", "cross_check_disparity", "fill_occlusions_disparity"});
 
     constexpr const int border = stereo_common::MAX_BORDER;
@@ -347,8 +188,6 @@ cv::Mat stereo_compute_disparity(const cv::Mat& left, const cv::Mat& right, int 
     REQUIRE(disparity <= std::numeric_limits<uchar>::max());
     REQUIRE(left.rows > stereo_common::MAX_BORDER);
     REQUIRE(left.cols > disparity);
-
-    set_parameters(program_str);
 
     const int rows = left.rows, cols = left.cols;
     constexpr const int border = stereo_common::MAX_BORDER;
