9c7c7ea65bf9923d8c4a6be560069a58355590db
agolubev <andpgolubev@gmail.com>
2020-04-14 19:27:18 +0300

    Introduce own threading stub to CMake and code

--- common/threading.hpp
+++ common/threading.hpp
@@ -1,7 +1,24 @@
 #pragma once
 
+#define NONE 0
+#define OPENMP 1
+#define SIMPLE 2
+#define ADVANCE 3
+
+#ifndef THREADS
+#define THREADS NONE
+#endif
+
 namespace thr {
-#ifdef OMP_THREADING
+#if THREADS == NONE
+template<typename T, typename Callable> inline void parallel_for(T iters, Callable f) {
+    for (T i = 0; i < iters; ++i) {
+        f(i, iters);
+    }
+}
+inline int get_max_threads() { return 1; }
+inline void set_num_threads(int n) { return; }
+#elif THREADS == OPENMP
 #include <omp.h>
 template<typename T, typename Callable> void parallel_for(T iters, Callable f) {
     if (iters == 1) {
@@ -14,7 +31,17 @@ template<typename T, typename Callable> void parallel_for(T iters, Callable f) {
 }
 inline int get_max_threads() { return omp_get_max_threads(); }
 inline void set_num_threads(int n) { omp_set_num_threads(n); }
-#else
+
+// TODO: implement other threading backends
+#elif THREADS == SIMPLE
+template<typename T, typename Callable> inline void parallel_for(T iters, Callable f) {
+    for (T i = 0; i < iters; ++i) {
+        f(i, iters);
+    }
+}
+inline int get_max_threads() { return 1; }
+inline void set_num_threads(int n) { return; }
+#elif THREADS == ADVANCE
 template<typename T, typename Callable> inline void parallel_for(T iters, Callable f) {
     for (T i = 0; i < iters; ++i) {
         f(i, iters);
